136bea7d718a1234b851169e2a6f238c2535424c
sunghoon <moonsh3222@gmail.com>
2023-07-03 00:16:47 +0200

    clean up buffer

--- circular_buffer.py
+++ circular_buffer.py
@@ -1,25 +1,54 @@
 from abc import ABC, abstractmethod
 
 class CircularBuffer(ABC):
+    """
+        This class represents a circular buffer
+
+        Properties:
+            buffer (list) : a buffer with the given size
+            write_index (int) : write index for the buffer
+            read_index (int) : read index for the buffer
+
+    """
     def __init__(self, size):
         self.buffer = [0] * size
-        self.index = 0
         self.write_index = 0
         self.read_index = 0
-        self.out = 0.0
-        self.delay = 1
 
-    def process_frame(self, input, delay):
+    def process_frame(self, buffer_input: float, delay: int) -> float:
+        """
+        This function process a frame of coming input using a processing line
+
+        Parameters:
+            buffer_input (float): input signal
+            delay: amount of sample delayed
+
+        Returns:
+            buffer_ouput (float): output signal
+
+        """
         self.read_index = self.write_index - delay
         if self.read_index < 0 :
             self.read_index = self.buffer
 
-        self.process_buffer(input, delay)
+        buffer_output = self.process_buffer(buffer_input, delay)
 
         self.write_index += 1
         if write_index > self.buffer:
             write_index = 0
 
+        return buffer_output
+
     @abstractmethod
-    def process_buffer(self, input, delay):
-        pass
+    def process_buffer(self, buffer_input : float, delay: int) -> float:
+        """
+        abtract method for processing line, implement a filter here.
+
+        Parameters:
+            buffer_input (float): input signal
+            delay: amount of sample delayed
+
+        Returns:
+            buffer_ouput (float): output signal
+
+        """
